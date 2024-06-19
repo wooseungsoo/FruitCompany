1c2a3828933e988f0dd1b1b1699f513a2d792d45
ErmaelStarius <ermael35@daum.net>
2024-06-19 17:37:06 +0900

    Add Scene
    처음 기본 프로그램 커밋

---
+++ Assets/Scripts/UI/UIConditions.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIConditions : MonoBehaviour
+{
+
+    public Conditions health;
+    public Conditions hunger;
+    public Conditions stamina;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharacterManager.Instance.Player.condition.uiCondition = this;
+    }
+}
